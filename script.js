109c294d4cc4b353a2944424e8943b32201391a8
RickeyBryant2002 <43036804+RickeyBryant2002@users.noreply.github.com>
2019-03-26 09:02:22 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,18 @@
+$(".driving").click(function(){
+$(".cart").toggle();
+console.log("hello");
+});
+    
+$("#button2").click(function(){
+$(".SuperMario").hide();
+console.log("hello");
+}); 
+    
+$(".mariokart").click(function(){
+$(".MarioParty").hide();
+console.log("hello");
+});
+    
+$("button").click(function(){
+    $("img").show();
+});
